059ec7b650d83e3fb1e639b9b2d89ff16fe5cf93
Tobias Studer <tobias.studer@unic.com>
2014-01-21 12:46:27 +0100

    Added sitecore courier wrapper for generating update package.

---
+++ build/New-SerializationPackage.ps1
@@ -0,0 +1,32 @@
+﻿Function New-SerializationPackage
+{
+    [CmdletBinding(
+    	SupportsShouldProcess=$True,
+        ConfirmImpact="Low"
+    )]
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$Source,
+        [Parameter(Mandatory=$true)]
+        [string]$Target,
+        [Parameter(Mandatory=$true)]
+        [string]$OutputFile
+    
+	)
+    Begin{}
+    
+    Process
+    {
+        $scriptInvocation = (Get-Variable MyInvocation -Scope 1).Value
+        $scriptPath = Split-Path $scriptInvocation.MyCommand.Definition -Parent
+
+        $Source = (Resolve-Path $Source -ErrorAction Stop).Path
+        $Target = (Resolve-Path $Target -ErrorAction Stop).Path
+
+        if(-not [System.IO.Path]::IsPathRooted($OutputFile)) {
+            $OutputFile = Join-Path $PWD $OutputFile
+        }
+
+        & "$scriptPath\..\tools\sitecore-courier\Sitecore.Courier.Runner.exe" /source:$Source /target:$Target /output:$OutputFile
+    }
+}
