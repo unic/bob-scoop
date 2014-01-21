58ea243172bf667b8e9eb8f01544fe80e119fb33
Tobias Studer <tobias.studer@unic.com>
2014-01-21 17:39:29 +0100

    Cleaned up psm1 file.

--- build/Build.psm1
+++ build/Build.psm1
@@ -1,7 +1,4 @@
 Get-ChildItem -Path $PSScriptRoot\*.ps1 -Exclude "init.ps1" | Foreach-Object{ . $_.FullName }
 
 Export-ModuleMember -Function * -Alias *
-
-Set-Alias csp Create-SerializationPackage
-
 Export-ModuleMember -Alias *
