33cef63731b10bcbc24327ab8650ba23f30a0986
Tobias Studer <tobias.studer@unic.com>
2014-01-27 14:01:18 +0100

    Added serialization scripts.

--- Install.psm1
+++ Install.psm1
@@ -1,4 +1,4 @@
-Get-ChildItem -Path $PSScriptRoot\*.ps1 -Exclude "init.ps1" | Foreach-Object{ . $_.FullName }
+Get-ChildItem -Path $PSScriptRoot\scripts\*.ps1 -Exclude "init.ps1" | Foreach-Object{ . $_.FullName }
 
 Export-ModuleMember -Function * -Alias *
 Export-ModuleMember -Alias *
---
+++ scripts/Publish-SerializationPackage.ps1
@@ -0,0 +1,45 @@
+﻿Function Publish-SerializationPackage
+{
+    [CmdletBinding(
+    	SupportsShouldProcess=$True,
+        ConfirmImpact="Low"
+    )]
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$Url,
+        [Parameter(Mandatory=$true)]
+        [string]$Mode,
+        [string]$Source,
+        [string]$Targets,
+        [string]$Lanugages
+    
+	)
+    Begin{}
+    
+    Process
+    {
+        $scriptInvocation = (Get-Variable MyInvocation -Scope 1).Value
+        $scriptPath = Split-Path $scriptInvocation.MyCommand.Definition -Parent
+
+        $params = @();
+        $params += "$url/services/publish/$Mode"
+        
+        $params += "--data"
+        $data = @();
+
+        if($Source) {
+            $data += "source=$Source"
+        }
+        if($Targets) {
+            $data += "targets=$Targets"
+        }
+        if($Lanugages) {
+            $data += "languages=$Lanugages"
+        }
+
+        $params += [string]::Join($data, "&");
+        
+        Start-Process "$scriptPath\..\lib\curl\curl.exe" $params -RedirectStandardOutput "$($env:TEMP)\install-serializationPackage-std.txt" -RedirectStandardError "$($env:TEMP)\install-serializationPackage-error.txt" -NoNewWindow  -Wait
+        Get-Content "$($env:TEMP)\install-serializationPackage-std.txt"
+    }
+}
