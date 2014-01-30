d2f967e6f013dea53508ba3ceaf1a1ad0e7ab612
Tobias Studer <tobias.studer@unic.com>
2014-01-30 16:50:11 +0100

    Ensure output directory.

--- scripts/New-SerializationPackage.ps1
+++ scripts/New-SerializationPackage.ps1
@@ -2,5 +2,7 @@
 $scriptPath = Split-Path $scriptInvocation.MyCommand.Definition -Parent
 $modulesPath = Join-Path $scriptPath 'modules'
 
+New-Item -ItemType Directory -Force -Path $args[2]
+
 Import-Module (Join-Path $modulesPath build) -Force
 New-SerializationPackage $args[0] $args[1] $args[2]
