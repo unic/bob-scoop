cbd28dd7b31b20a0210f100392a771acc8d975cb
Tobias Studer <tobias.studer@unic.com>
2014-01-30 17:01:49 +0100

    Create the output directory correctly.

--- scripts/New-SerializationPackage.ps1
+++ scripts/New-SerializationPackage.ps1
@@ -2,7 +2,8 @@
 $scriptPath = Split-Path $scriptInvocation.MyCommand.Definition -Parent
 $modulesPath = Join-Path $scriptPath 'modules'
 
-New-Item -ItemType Directory -Force -Path $args[2]
+$outputPath = Split-Path $args[2] -Parent
+New-Item -ItemType Directory -Force -Path $outputPath
 
 Import-Module (Join-Path $modulesPath build) -Force
 New-SerializationPackage $args[0] $args[1] $args[2]
