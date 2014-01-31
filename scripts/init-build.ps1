6bed3dbcc8b2a35e8a51bfbab54f722e2b9c3556
Tobias Studer <tobias.studer@unic.com>
2014-01-31 13:38:44 +0100

    Clean-up of nuspec.

--- scripts/init-build.ps1
+++ scripts/init-build.ps1
@@ -1,4 +1,4 @@
 param($installPath, $toolsPath, $package)
 
-$modulesPath = Join-Path $toolsPath 'modules'
+$modulesPath = Join-Path $toolsPath 'scripts\modules'
 Import-Module (Join-Path $modulesPath build)
