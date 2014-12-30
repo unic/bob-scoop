b8a2b3e8d9c39b7a2e4fd7e96c1c9db3b648c62a
Yannis Guedel <yannis.guedel@unic.com>
2014-12-30 13:31:08 +0100

    updated import-scdatabases docs

--- src/tools/Scoop/Import-ScDatabases.ps1
+++ src/tools/Scoop/Import-ScDatabases.ps1
@@ -1,10 +1,11 @@
 <#
 .SYNOPSIS
-Restores all Databases of a project from a file share.
+Restores all databases of a project from a file share.
 .DESCRIPTION
-Restores all Databases which are referenced in the ConnectionStrings file of the Project.
-The location where the backups to restore are taken must be configured in the Bob.config file.
-If a database already exists it will be replaced. If not it will be created at the default location or in the DatabasePath.
+Restores all databases which are referenced in the ConnectionStrings file of the project to the local database server.
+The backup will be copied to the $env:TEMP directory before restoring it.
+The location of the backups to restore must be configured in the Bob.config file.
+If a database allready exists it will be replaced. If not it will be created at the default location or in the DatabasePath.
 
 
 .PARAMETER ConnectionStringsFile
