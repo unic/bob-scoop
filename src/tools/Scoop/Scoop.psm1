c77bf286adc349752e112d9671613f47dec3a704
Yannis Guedel <yannis.guedel@unic.com>
2014-12-31 10:36:01 +0100

    set default verbose action to output

--- src/tools/Scoop/Scoop.psm1
+++ src/tools/Scoop/Scoop.psm1
@@ -18,3 +18,4 @@ $partentPath = (Get-Item $PSScriptRoot).Parent.FullName
 
 Get-ChildItem -Path $PSScriptRoot\*.ps1 -Exclude "*.Tests.ps1" | Foreach-Object{ . $_.FullName }
 Export-ModuleMember -Function * -Alias *
+$VerbosePreference = "Continue"
