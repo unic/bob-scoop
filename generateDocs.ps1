7e5c9123cc23175f2707b777a6295b339a3d6861
Yannis Guedel <yannis.guedel@unic.com>
2015-01-09 17:39:40 +0100

    adapated generateDocs for TeamCity

--- generateDocs.ps1
+++ generateDocs.ps1
@@ -7,6 +7,10 @@ Import-Module "$PSScriptRoot\src\tools\$module" -Force
 
 New-PsDoc -Module $module -Path "$PSScriptRoot\docs\" -OutputLocation "$PSScriptRoot\docs-generated"
 
-gitbook build "$PSScriptRoot\docs-generated\"
+New-PsDoc -Module $module -Path "$PSScriptRoot\docs\" -OutputLocation "$PSScriptRoot\docs-generated" -Verbose
+New-GitBook "$PSScriptRoot\docs-generated" "$PSScriptRoot\temp" $username $password -Buildserver:$Buildserver
+
+
+
 
 
