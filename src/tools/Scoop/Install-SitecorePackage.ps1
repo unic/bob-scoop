7ea5ccaf4f4fbe1a41a698ea264aa24e02aced5e
Yannis Guedel <yannis.guedel@unic.com>
2015-01-12 14:24:51 +0100

    documented install-sitecorepackage

--- src/tools/Scoop/Install-SitecorePackage.ps1
+++ src/tools/Scoop/Install-SitecorePackage.ps1
@@ -1,3 +1,18 @@
+<#
+.SYNOPSIS
+Installs the correct Sitecore distribution to a specific location.
+.DESCRIPTION
+Install-SitecorePackage installs the  correct Sitecore distribution to a specific location.
+The Sitecore version is calculated by looking for Sitecore.Mvc.Config
+or Sitecore.WebForms.Config installed to the Website proje
+
+.PARAMETER OutputLocation
+The location where to extract the Sitecore distribution
+
+.EXAMPLE
+Install-SitecorePackage -OutputLocation D:\Web\Magic
+
+#>
 function Install-SitecorePackage
 {
     [CmdletBinding()]
