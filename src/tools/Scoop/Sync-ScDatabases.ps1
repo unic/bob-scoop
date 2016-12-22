a2d099197e83dbb14f3fe604baea14c4ab6fd045
Yannis Güdel <yannis.guedel@unic.com>
2016-12-22 09:36:46 +0100

    added small debug statement

--- src/tools/Scoop/Sync-ScDatabases.ps1
+++ src/tools/Scoop/Sync-ScDatabases.ps1
@@ -74,7 +74,7 @@ function Sync-ScDatabases
         $indexes = $config.PulishIndexes
         if($indexes) {
                 $rebuildIndexUrl = "$baseUrl/bob/rebuildIndexes?indexes=$indexes"
-                Write-Verbose "Rebuild indexes: $indexes"
+                Write-Verbose "Rebuild indexes: $indexes with url $rebuildIndexUrl"
                 $result = Invoke-WebRequest -Uri $rebuildIndexUrl -Headers @{ "Authenticate" = $deploymentToolAuthToken } -TimeoutSec 10800 -UseBasicParsing
                 $result.Content
         }
